acb9b7e17d70da7754944dc1e500c1e5742ed137
cheesejacker <challenbonjour@yahoo.com>
2020-11-26 11:49:36 -0800

    Fixes #26 Fixes #7

--- op_header/custom.hpp
+++ op_header/custom.hpp
@@ -12,10 +12,10 @@ class Custom: public Base{
 		Custom(Base* l, Base* r, string custom_op) : l(l), r(r) {
 			set_operation(custom_op);
 		}
-		virtual double evaluate(); // TODO
+		virtual double evaluate();
 		virtual bool has_inverse() { return true; }
 		virtual bool is_closed() { return true; }
-		virtual bool is_inverse(Base *a) { return true; }
+		virtual bool is_inverse(Base *a, Base *e = 0) { return true; }
 		void set_operation(string ); //helper function for evaluate()
 	private:
 		Base* l;
