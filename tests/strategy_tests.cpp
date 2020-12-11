86ae2d9dca5c5613b51ff8b77b7c8e75d82927b4
Angela-Su <i.am.angela.su@gmail.com>
2020-12-11 10:12:54 -0800

    finished closure.cpp and tested :)

--- tests/strategy_tests.cpp
+++ tests/strategy_tests.cpp
@@ -59,13 +59,55 @@ TEST(IdentityTest, T5){
         EXPECT_EQ(v->getIdentity(), nullptr);
 }
 
-/*TEST(AssociativeTest, T1){
-	set<Op*>* x = new set <Op*> {
-		new Op(1), new Op(5), new Op(9)};
-	string op = "+";
-	Group* g = new Group(x, op);
-	//check associative
-}*/
+TEST(ClosureTest, T1){
+        set<Op*>* x = new set <Op*> {
+                new Op(1), new Op(3), new Op(5), new Op(7)};
+        string op = "modm8";
+        Group* g = new Group(x, op);
+        Verifier* v = new Closure();
+        v->setGroup(g);
+        EXPECT_EQ(v->verify(), true);
+}
+
+TEST(ClosureTest, T2){ 
+        set<Op*>* x = new set <Op*> {
+                new Op(1), new Op(2), new Op(3)};
+        string op = "modp6";
+        Group* g = new Group(x, op);
+        Verifier* v = new Closure();
+        v->setGroup(g);
+        EXPECT_EQ(v->verify(), false);
+}
+
+TEST(ClosureTest, T3){
+        set<Op*>* x = new set <Op*> {
+                new Op(0), new Op(1)};
+        string op = "+";
+        Group* g = new Group(x, op);
+        Verifier* v = new Closure();
+        v->setGroup(g);
+        EXPECT_EQ(v->verify(), false);
+}
+
+TEST(ClosureTest, T4){
+        set<Op*>* x = new set <Op*> {
+                new Op(1), new Op(2)};
+        string op = "a*b/2";
+        Group* g = new Group(x, op);
+        Verifier* v = new Closure();
+        v->setGroup(g);
+        EXPECT_EQ(v->verify(), false);
+}
+
+TEST(ClosureTest, T5){
+        set<Op*>* x = new set <Op*> {
+                new Op(1), new Op(3)};
+        string op = "a/b+2";
+        Group* g = new Group(x, op);
+        Verifier* v = new Closure();
+        v->setGroup(g);
+        EXPECT_EQ(v->verify(), false);
+}
 
 int main(int argc, char **argv){
 	::testing::InitGoogleTest(&argc, argv);
